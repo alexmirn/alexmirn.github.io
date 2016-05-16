7d58f22ca3ebc348efe03a7deb8140ebd0622b75
Alex Mirniy <alex_mirn@ukr.net>
2016-05-16 09:50:33 +0300

    added dropdown menu js9-10

---
+++ js9-10/js/jquery.menu.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $( '.dropdown' ).hover(
+        function(){
+            $(this).children('.sub-menu').slideDown(200);
+            $(this).children('.sub-menu').animate({backgroundColor:"violet"}, 500 );
+        },
+        function(){
+            $(this).children('.sub-menu').slideUp(200);
+            $(this).children('.sub-menu').animate({backgroundColor:"yellow"}, 500 );
+        }
+    );
+}); // end ready
+
+
