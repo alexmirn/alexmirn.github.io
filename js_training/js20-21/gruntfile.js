75a9bc7916efc68981d283c26e3c36e1aaf7b7bb
Alex Mirniy <alex_mirn@ukr.net>
2016-06-25 13:55:44 +0300

    js20-21 train

--- js_training/js20-21/gruntfile.js
+++ js_training/js20-21/gruntfile.js
@@ -1,6 +1,22 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    babel: {
+        options: {
+            sourceMap: false,
+            presets: ['es2015']
+        },
+        dist: {
+            files: [{
+                expand: true,
+                cwd: 'js/src',
+                src: ['*.js'],
+                dest: 'js/dist',
+                ext: '.js',
+                extDot: 'first'
+            }]
+        }
+    },
     concat: {
       options: {
         separator: ';'
