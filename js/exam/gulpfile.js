09d0a21e4efb2d8ba048f9940d15dee10b9738b6
alexmirn <alex_mirn@ukr.net>
2016-07-09 16:25:08 +0300

    exam fixes

--- js/exam/gulpfile.js
+++ js/exam/gulpfile.js
@@ -7,12 +7,8 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
-    sourcemaps = require('gulp-sourcemaps'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    rimraf = require('rimraf'),
-    browserSync = require("browser-sync"),
-    reload = browserSync.reload;
+    sourcemaps = require('gulp-sourcemaps');
+
 
 var path = {
     dist: {
@@ -39,29 +35,10 @@ var path = {
     clean: './dist'
 };
 
-var config = {
-    server: {
-        baseDir: "./dist"
-    },
-    tunnel: true,
-    host: 'localhost',
-    port: 8000,
-    logPrefix: "Test_task"
-};
-
-gulp.task('webserver', function () {
-    browserSync(config);
-});
-
-gulp.task('clean', function (cb) {
-    rimraf(path.clean, cb);
-});
-
 gulp.task('html:build', function () {
     gulp.src(path.src.html)
         .pipe(rigger())
         .pipe(gulp.dest(path.dist.html))
-        .pipe(reload({stream: true}));
 });
 
 gulp.task('js:build', function () {
@@ -71,7 +48,6 @@ gulp.task('js:build', function () {
         .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.dist.js))
-        .pipe(reload({stream: true}));
 });
 
 gulp.task('style:build', function () {
@@ -84,19 +60,11 @@ gulp.task('style:build', function () {
         .pipe(prefixer())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.dist.css))
-        .pipe(reload({stream: true}));
 });
 
 gulp.task('image:build', function () {
     gulp.src(path.src.img)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()],
-            interlaced: true
-        }))
         .pipe(gulp.dest(path.dist.img))
-        .pipe(reload({stream: true}));
 });
 
 gulp.task('fonts:build', function() {
@@ -132,4 +100,4 @@ gulp.task('watch', function(){
 });
 
 
-gulp.task('default', ['build', 'webserver', 'watch']);
+gulp.task('default', ['build','watch']);
