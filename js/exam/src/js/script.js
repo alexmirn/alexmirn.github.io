fdb65ea4a29f0b72c40ad1557e5714619b8d2165
Oleksiy Myrnyy <Oleksiy Myrnyy>
2016-08-11 08:29:04 +0300

    added masonry for ie8

--- js/exam/src/js/script.js
+++ js/exam/src/js/script.js
@@ -4,22 +4,24 @@ $(function () {
             auto: false,
             autoControls: true
         });
-
-            //Works fine in IE9+, FF and Chrome. 
-//dataColumn = jQuery('.table-header div.rf-edt-hdr + div table table tbody > tr:nth-child(1) td:nth-child(1)');
-//headerColumn = jQuery('div.table-header > div.rf-edt-hdr table table > tbody > tr td:nth-child(1)');
-dataColumn = jQuery('.table-header div.rf-edt-hdr + div table table tbody > tr').eq(0).find('td').eq(0);
-headerColumn = jQuery('div.table-header > div.rf-edt-hdr table table > tbody > tr td').eq(0);
         
         function grid() {
+            // $('.grid').masonry({
+            //   columnWidth: 200,
+            //   itemSelector: '.grid-item'
+            // });
+
+
+            // var $container = $('#container').masonry();
+            //   $container.imagesLoaded( function() {
+            //     $container.masonry();
+            //   });
+
             var $grid = $('.grid');
             $grid.imagesLoaded(function () {
                 $grid.masonry({
                     itemSelector: '.grid-item',
-                    columnWidth: function( containerWidth ) {
-                        return (containerWidth / 3 - 10)
-                    },
-                    gutterWidth: 10
+                    columnWidth: 10
                 });
             });
         }
@@ -27,21 +29,35 @@ headerColumn = jQuery('div.table-header > div.rf-edt-hdr table table > tbody > t
         $('.grid').masonry({
           // options
           itemSelector: '.grid-item',
-          columnWidth: 200
+          columnWidth: 10
         });
         
         function search() {
-            $('.ideas').find('div').remove();
+            // $('.ideas').find('div').remove();
             var $searchKey = $('.search__field').val();
-
+            // jQuery.support.cors = true;
+            
             $.ajax({
                 url: 'https://pixabay.com/api/?key=2650584-e8feab9b11ed644dcec728ac9&q=' + $searchKey + '&image_type=photo',
+                // url: 'https://pixabay.com/api/?key=2650584-e8feab9b11ed644dcec728ac9&q=' + $searchKey + '&image_type=photo',
+                // url: 'https://api.pixplorer.co.uk/image?word=' + $searchKey + '&amount=10',
                 dataType: 'jsonp',
                 success: function (data) {
-
-                    var $html = $('#container').html();
-                    var $content = tmpl($html, data);
-                    $('.ideas').append($content);
+                    // var $html = $('#container').html();
+                    // var $content = tmpl($html, data);
+                        // var x = $(".ideas__pic__img");
+                        var images = document.querySelectorAll(".ideas__pic__img");
+                        console.log(images.length);
+                        
+                        var x = document.querySelectorAll(".ideas__pic__img");
+                        var y = document.querySelectorAll(".ideas__pic__title");
+                        console.log(x)
+                    for (var i = 0; i < 9; i++) {
+                        x[i].setAttribute('src', data.hits[i].webformatURL);
+                        // y[i].innerHTML = data.hits[i].tags.split(', ')[1];
+                    }
+                    console.log('alesha');
+                    // $('.ideas').append($content);
                     grid();
                 },
                 error: function () {
--- js/js15-16/js/script.js
+++ js/js15-16/js/script.js
@@ -13,63 +13,170 @@ $(function () {
         e.preventDefault();
         $('.result img').remove();
         var $searchKey = $('.search').val();
-        $.ajax({
-            url: 'https://pixabay.com/api/?key=' + API_KEY + '&q='+ $searchKey + '&image_type=photo',
-            dataType: 'jsonp',
-            success: function (data) {
-               var $results = data.hits;
-               for (var i=0; i < $results.length; i++) {
-                    $('.result').append('<img src="' + $results[i].previewURL + '">');
-               }
-           },
-           error: function(){
-            alert('Error request')
-            }
-        });
+
+        // if ('XDomainRequest' in window && window.XDomainRequest !== null) {
+
+        //     var xdr = new XDomainRequest(); // Use Microsoft XDR
+        //     xdr.open('get', 'https://api.riffsy.com/v1/search?tag=alex');
+        //     xdr.onload = function () {
+        //         var dom  = new ActiveXObject('Microsoft.XMLDOM'),
+        //             data = $.parseJSON(xdr.responseText);
+
+        //         dom.async = false;
+
+        //         console.log(data);
+        //            var $results = data.results;
+        //            console.log($results);
+        //            for (var i=0; i < $results.length; i++) {
+        //                 // $('.result').append('<img src="' + $results[i].previewURL + '">');
+        //                 $('.result').append('<img src="' + $results[i].url + '">');
+        //                 // $('.result').append('<img src="' + $results[i].media[0].preview + '">');
+        //            }
+
+        //         // if (JSON == null || typeof (JSON) == 'undefined') {
+        //         //     JSON = $.parseJSON(data.firstChild.textContent);
+        //         // }
+
+        //         successCallback(data); // internal function
+        //     };
+
+        //     xdr.onerror = function() {
+        //         _result = false;  
+        //     };
+
+        //     xdr.send();
+        // } else { 
+            jQuery.support.cors = true;
+            $.ajax({
+                url: 'https://pixabay.com/api/?key=' + API_KEY + '&q='+ $searchKey + '&image_type=photo',
+                // url: 'https://api.riffsy.com/v1/search?tag=' + $searchKey + '&key=LIVDSRZULELA',
+        //         type: "GET",
+        //         xhr:  (window.ActiveXObject) ?
+        // function() {
+        //         try {
+        //             return new window.ActiveXObject("Microsoft.XMLHTTP");
+        //         } catch(e) {}
+        //     } :
+        //     function() {
+        //         return new window.XMLHttpRequest();
+        //     },
+                // url: 'https://api.riffsy.com/v1/search?tag=' + $searchKey,
+                // dataType: 'json',
+                dataType: 'jsonp',
+                success: function (data) {
+                   // var $results = data.hits;
+
+                   updCont(data)
+                   
+               },
+               error: function(){
+                alert('Error request')
+                }
+            });
+        }
         $('.search').val('');
-    }
+
+        function updCont(data){
+            console.log(data);
+            var $results = data.hits;
+                   // var $results = data.results;
+                   console.log($results);
+                   for (var i=0; i < $results.length; i++) {
+                        $('.result').append('<img src="' + $results[i].previewURL + '">');
+                        // $('.result').append('<img src="' + $results[i].url + '">');
+                        // $('.result').append('<img src="' + $results[i].media[0].preview + '">');
+                   }
+        }
+
+        
+    // }
+
+
+ // if ('XDomainRequest' in window && window.XDomainRequest !== null) {
+
+ //        var xdr = new XDomainRequest(); // Use Microsoft XDR
+ //        xdr.open('get', url);
+ //        xdr.onload = function () {
+ //            var dom  = new ActiveXObject('Microsoft.XMLDOM'),
+ //                JSON = $.parseJSON(xdr.responseText);
+
+ //            dom.async = false;
+
+ //            if (JSON == null || typeof (JSON) == 'undefined') {
+ //                JSON = $.parseJSON(data.firstChild.textContent);
+ //            }
+
+ //            successCallback(JSON); // internal function
+ //        };
+
+ //        xdr.onerror = function() {
+ //            _result = false;  
+ //        };
+
+ //        xdr.send();
+ //    } 
+
+ //    // IE7 and lower can't do cross domain
+ //    else if (navigator.userAgent.indexOf('MSIE') != -1 &&
+ //             parseInt(navigator.userAgent.match(/MSIE ([\d.]+)/)[1], 10) < 8) {
+ //       return false;
+ //    }    
+
+ //    // Do normal jQuery AJAX for everything else          
+ //    else {
+ //        $.ajax({
+ //            url: url,
+ //            cache: false,
+ //            dataType: 'json',
+ //            type: 'GET',
+ //            async: false, // must be set to false
+ //            success: function (data, success) {
+ //                successCallback(data);
+ //            }
+ //        });
+ //    }
 
     // OOP task
-function Human(name, age, state, height, weight) {
-    this.name = name;
-    this.age = age;
-    this.state = state;
-    this.height = height;
-    this.weight = weight;
-}
+// function Human(name, age, state, height, weight) {
+//     this.name = name;
+//     this.age = age;
+//     this.state = state;
+//     this.height = height;
+//     this.weight = weight;
+// }
 
-var human = new Human('Alex', 29, 'male', 181, 81);
-console.log(human);
-console.log('--------------------------------');
+// var human = new Human('Alex', 29, 'male', 181, 81);
+// console.log(human);
+// console.log('--------------------------------');
 
-function Worker(name, age, state, height, weight, company, salary){
-    Human.apply(this, arguments);
-    this.company = company;
-    this.salary = salary;
-    this.work = function(){
-        console.log(this.name + ' works');
-    }
-}
-Worker.prototype = human;
+// function Worker(name, age, state, height, weight, company, salary){
+//     Human.apply(this, arguments);
+//     this.company = company;
+//     this.salary = salary;
+//     this.work = function(){
+//         console.log(this.name + ' works');
+//     }
+// }
+// Worker.prototype = human;
 
-var newWorker = new Worker('Yurii', 22, 'male', 183, 87, 'Google', 10000);
-console.log(newWorker);
-newWorker.work();
-console.log('--------------------------------');
+// var newWorker = new Worker('Yurii', 22, 'male', 183, 87, 'Google', 10000);
+// console.log(newWorker);
+// newWorker.work();
+// console.log('--------------------------------');
 
-function Student(name, age, state, height, weight, university, award){
-    Human.apply(this, arguments);
-    this.university = university;
-    this.award = award;
-    this.watchSeries = function(){
-        console.log(this.name + ' watches TV series');
-    }
-}
-Student.prototype = human;
+// function Student(name, age, state, height, weight, university, award){
+//     Human.apply(this, arguments);
+//     this.university = university;
+//     this.award = award;
+//     this.watchSeries = function(){
+//         console.log(this.name + ' watches TV series');
+//     }
+// }
+// Student.prototype = human;
 
-var newStudent = new Student('Yana', 26, 'feemale', 183, 87, 'Harvard', 5000);
-console.log(newStudent);
-newStudent.watchSeries();
+// var newStudent = new Student('Yana', 26, 'feemale', 183, 87, 'Harvard', 5000);
+// console.log(newStudent);
+// newStudent.watchSeries();
 
 });
 
